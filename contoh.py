c92db7095fb1480956825f09e7c994dfebef2f10
Suryadharmaa <surya.dharma.ac@gmail.com>
2026-05-08 09:28:43 +0700

    Create contoh.py

---
+++ contoh.py
@@ -0,0 +1,15 @@
+''' sitem pengkasiran '''
+
+pelanggan = 1
+
+while pelanggan < 2:
+  print(f'Pelanggan ke-(pelanggan)')
+  total = 0
+
+while = True:
+  harga = int(input("masukkan harga barang : "))
+  if harga == 0:
+    break
+  total += harga
+print("total : ", total)
+pelanggan += 1
