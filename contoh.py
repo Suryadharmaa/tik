6d15b9f1851bdf5fcc615e670e7f68a0dfa73345
Suryadharmaa <surya.dharma.ac@gmail.com>
2026-05-08 09:33:46 +0700

    Update contoh.py

--- contoh.py
+++ contoh.py
@@ -3,10 +3,10 @@
 pelanggan = 1
 
 while pelanggan < 2:
-  print(f'Pelanggan ke-(pelanggan)')
+  print(f'Pelanggan ke-{pelanggan}')
   total = 0
 
-while = True:
+while True:
   harga = int(input("masukkan harga barang : "))
   if harga == 0:
     break
